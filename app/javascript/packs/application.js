6beb7f72740b5174a7ac9d75c7a5cec1375e0d3d
nayness <nayness@gmail.com>
2021-10-10 22:59:32 -0300

    Add: form validation

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 import 'bootstrap'
+import 'jquery-validation'
 import 'packs/characters'
 
 Rails.start()
--- app/javascript/packs/characters.js
+++ app/javascript/packs/characters.js
@@ -4,7 +4,34 @@ $( document ).on('turbolinks:load', function() {
   showSliderValue("strenght");
   showSliderValue("spirit");
   showSliderValue("charisma");
+  validateCharacter();
 })
+
+function validateCharacter(){
+  $("#new_character").validate({
+    onkeyup: true,
+    errorClass: 'is-invalid',
+    errorElement: 'div',
+    rules: {
+      'character[name]': "required",
+      'character[power]': "required",
+      'character[attack]': "required",
+      'character[spirit]': "required",
+      'character[charisma]': "required",
+      'character[strenght]': "required",
+      'character[special_skill]': "required",
+    },
+    messages: {
+      'character[name]': {
+        required: 'Tienes que ingresar un nombre'
+      },
+      'character[special_skill]': {
+        required: 'Tienes elegir una habilidad especial'
+      },
+    }
+   })
+}
+
 function showSliderValue(name){
   $("#character_" + name).on('input', function(){
     console.log($(this).val())
